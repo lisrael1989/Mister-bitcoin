4e296f9f10419fd744d4392067dc4430e85f84c1
israel <lisrael1989@gmail.com>
2024-05-10 15:47:22 +0300

    adding routs to app

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import ContactIndex from "../views/ContactIndex.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +11,23 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: "/contact",
+      name: "contact",
+      component: ContactIndex,
+    },
+    {
+      path: "/contact/:contactId",
+      name: "ContactDetails",
+      component: ContactDetails,
+    },
+    {
+      path: "/contact/edit/:contactId?",
+      name: "contactEdit",
+      component: ContactEdit,
+    },
+    {
       path: "/Statistics",
       name: "Statistics",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/Statistics.vue"),
     },
   ],
