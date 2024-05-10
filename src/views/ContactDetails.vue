21096e81843679caf0949c7b8197777161df7e73
israel <lisrael1989@gmail.com>
2024-05-10 15:49:04 +0300

    adding view of contact details

---
+++ src/views/ContactDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2 v-if="!contact">Loading...</h2>
+  <section v-else class="contact-details">
+    <h2>{{ contact.name }}</h2>
+    <h2>{{ contact.email }}</h2>
+    <h2>{{ contact.phone }}</h2>
+    <RouterLink to="/contact"><button>Back</button></RouterLink>
+  </section>
+</template>
+
+<script>
+import { contactService } from "@/services/contact.service.js"
+
+export default {
+  data() {
+    return {
+      contact: null,
+    }
+  },
+  async created() {
+    const { contactId } = this.$route.params
+
+    this.contact = await contactService.get(contactId)
+  },
+}
+</script>
+
+<style lang="scss"></style>
