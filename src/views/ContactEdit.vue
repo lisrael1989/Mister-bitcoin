fcd166a2be982fc9f55551aa9eeb2b6aade5399d
israel <lisrael1989@gmail.com>
2024-05-10 15:48:49 +0300

    adding cmp to edit contact

---
+++ src/views/ContactEdit.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2 v-if="!contact">Loading...</h2>
+  <form v-else @submit.prevent="save" class="contact-edit">
+    <input v-model="contact.name" type="text" placeholder="name" />
+    <input v-model="contact.email" type="email" placeholder="email" />
+    <input v-model="contact.phone" type="number" placeholder="phone" />
+    <button>Save</button>
+  </form>
+</template>
+
+<script>
+import { contactService } from "@/services/contact.service"
+// import { showSuccessMsg } from "@/services/event-bus.service"
+
+export default {
+  data() {
+    return {
+      contact: null,
+    }
+  },
+  methods: {
+    async save() {
+      await contactService.save(this.contact)
+      showSuccessMsg("contact saved.")
+      this.$router.push("/contact")
+    },
+  },
+  async created() {
+    const { contactId } = this.$route.params
+
+    if (contactId) {
+      this.contact = await contactService.get(contactId)
+    } else {
+      this.contact = contactService.getEmptyContact()
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.contact-edit {
+  display: grid;
+  justify-items: start;
+  gap: 10px;
+
+  padding: 10px;
+  background-color: rgb(245, 224, 145);
+
+  button {
+    place-self: end;
+  }
+}
+</style>
