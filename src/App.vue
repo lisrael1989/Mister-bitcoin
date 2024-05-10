2fce1c7355808dec009780c572ce9206129530b4
israel <lisrael1989@gmail.com>
2024-05-10 13:37:33 +0300

    Setup mister bitcoin app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from "./cmps/AppHeader.vue"
+
+export default {
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/cmps/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="app-header">
+    <h1>🪙Mr. Bitcoin</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/car">Contacts</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.app-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+  font-weight: 600;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
